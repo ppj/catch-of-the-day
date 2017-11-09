494cff9d1a71fdd7a1ff60f56f62fd8767d3392b
Prasanna Joshi <prasanna_3003@yahoo.com>
2017-11-10 01:49:01 +1100

    Unstyled store picker form

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,7 +2,13 @@ import React from "react";
 
 class StorePicker extends React.Component {
   render() {
-    return <p>Hello</p>
+    return (
+      <form className="store-picker">
+        <h2>Please enter a store name</h2>
+        <input type="text" required placeholder="Store name" />
+        <button type="submit">Visit 🏃</button>
+      </form>
+    )
   }
 }
 
