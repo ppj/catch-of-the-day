c0d80aed9e6976b97a6aa80f518c961ccac90d7c
Prasanna Joshi <prasanna_3003@yahoo.com>
2017-11-13 02:03:41 +1100

    Style the heading & tagline

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,18 @@ import React from "react";
 class Header extends React.Component {
   render() {
     return(
-      <header>
-        <h1>Catch of the Day</h1>
-        <h3>{this.props.tagline}</h3>
+      <header className="top">
+        <h1>
+          Catch
+          <span className="ofThe">
+            <span className="of">of</span>
+            <span className="the">the</span>
+          </span>
+          Day
+        </h1>
+        <h3 className="tagline">
+          <span>{this.props.tagline}</span>
+        </h3>
       </header>
     )
   }
