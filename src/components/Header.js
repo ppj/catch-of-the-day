f634128ef2b85a67189cdc20056245f593c6047f
Prasanna Joshi <prasanna_3003@yahoo.com>
2017-11-13 01:59:36 +1100

    Pass tagline as a prop to make it dynamic

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,10 @@ import React from "react";
 class Header extends React.Component {
   render() {
     return(
-      <p>Header</p>
+      <header>
+        <h1>Catch of the Day</h1>
+        <h3>{this.props.tagline}</h3>
+      </header>
     )
   }
 }
