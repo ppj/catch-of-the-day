e96afda3bb0f1495b28a65542a927b5d4cb6fe0b
Prasanna Joshi <prasanna_3003@yahoo.com>
2017-11-15 01:20:15 +1100

    Add basic react router & match components to root & store paths

--- src/index.js
+++ src/index.js
@@ -3,7 +3,20 @@
 import React from "react";
 import "./css/style.css"
 import { render } from "react-dom";
+import { BrowserRouter, Match } from 'react-router';
+
 import App from "./components/App"
 import StorePicker from "./components/StorePicker"
 
-render(<App/>, document.querySelector('#main'));
+const Root = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Match exactly pattern="/" component={StorePicker} />
+        <Match pattern="/store/:storeId" component={App} />
+      </div>
+    </BrowserRouter>
+  )
+}
+
+render(<Root/>, document.querySelector('#main'));
