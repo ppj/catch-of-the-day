be841916fe6faa689017badf6ec75d5099f8ee8a
Prasanna Joshi <prasanna_3003@yahoo.com>
2017-11-24 01:14:29 +1100

    Add router to context & transition to entered store name

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -5,7 +5,9 @@ import { getFunName } from "../helpers"
 class StorePicker extends React.Component {
   goToStore(event) {
     event.preventDefault()
-    console.log(this.storeInput.value)
+    const storeId = this.storeInput.value
+    console.log(`Going to ${storeId}`)
+    this.context.router.transitionTo(`/store/${storeId}`)
   }
 
   render() {
@@ -18,6 +20,10 @@ class StorePicker extends React.Component {
       </form>
     )
   }
+}
+
+StorePicker.contextTypes = {
+  router: React.PropTypes.object
 }
 
 export default StorePicker;
