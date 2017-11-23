8ab8915c8334047996904ccebac88868ba424d3e
Prasanna Joshi <prasanna_3003@yahoo.com>
2017-11-24 00:43:25 +1100

    trigger simple console msg on form submission

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -3,9 +3,13 @@ import React from "react";
 import { getFunName } from "../helpers"
 
 class StorePicker extends React.Component {
+  goToStore() {
+    console.log("You changed the URL dude!")
+  }
+
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={ this.goToStore }>
         <h2>Please enter a store</h2>
         <input type="text" required placeholder="Store name" defaultValue={getFunName()} />
         <button type="submit">Visit 🚘</button>
