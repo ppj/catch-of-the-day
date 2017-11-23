93c54e77571678448c85da43938ab4ac53b413b0
Prasanna Joshi <prasanna_3003@yahoo.com>
2017-11-24 00:50:32 +1100

    constructor way of binding function to object

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -3,16 +3,22 @@ import React from "react";
 import { getFunName } from "../helpers"
 
 class StorePicker extends React.Component {
+  constructor() {
+    super()
+    this.goToStore = this.goToStore.bind(this)
+  }
+
   goToStore(event) {
     event.preventDefault()
-    console.log("You changed the URL dude!")
+    console.log(this.storeInput)
   }
 
   render() {
     return (
       <form className="store-selector" onSubmit={ this.goToStore }>
         <h2>Please enter a store</h2>
-        <input type="text" required placeholder="Store name" defaultValue={getFunName()} />
+        <input type="text" required placeholder="Store name" defaultValue={getFunName()}
+          ref={ (input) => { this.storeInput = input } }/>
         <button type="submit">Visit 🚘</button>
       </form>
     )
